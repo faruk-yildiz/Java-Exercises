8d58ab6c9f333c68e4b92f3da8a6f2f264582606
Faruk <omerfarukyiildiz@gmail.com>
2021-07-20 11:20:37 +0300

    First commit

---
+++ KilogramToPound/KilogramToPound.java
@@ -0,0 +1,9 @@
+public class KilogramToPound {
+  public static void main(String[] args) {
+
+    System.out.println("Kilogram \t Pound");
+    for (int i=1;i<=199;i++){
+      System.out.printf(" %d \t %.1f \n",i,(i*2.2));
+    }
+  }
+}
