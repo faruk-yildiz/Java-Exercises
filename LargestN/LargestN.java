7ac4be751a303e20fb32c717b43843c71af19504
Faruk <omerfarukyiildiz@gmail.com>
2021-07-20 15:15:56 +0300

    First Commit

---
+++ LargestN/LargestN.java
@@ -0,0 +1,11 @@
+public class LargestN {
+  public static void main(String[] args) {
+    int n=1;
+    int largestN=1;
+    while (n*n*n<12000) {
+      largestN=n;
+      n++;
+    }
+    System.out.println("Smallest n such that n^3<12000 is "+largestN);
+  }
+}
