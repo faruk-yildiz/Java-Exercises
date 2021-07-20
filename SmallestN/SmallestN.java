122dc03cc238da7b2598a07801b58796db3e591d
Faruk <omerfarukyiildiz@gmail.com>
2021-07-20 15:11:34 +0300

    Code updated

--- SmallestN/SmallestN.java
+++ SmallestN/SmallestN.java
@@ -2,11 +2,9 @@
 public class SmallestN {
   public static void main(String[] args) {
     int n=1;
-    int smallestN=1;
     while (n*n<12000) {
-      smallestN=n;
       n++;
     }
-    System.out.println("Smallest n such that n^2<12000 is "+smallestN);
+    System.out.println("Smallest n such that n^2<12000 is "+n);
   }
 }
